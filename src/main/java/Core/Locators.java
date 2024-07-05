adc09d55dc53ee8dbe4e72f4c8318f0545d90e7e
NarcisKG <dusan224@gmail.com>
2024-07-05 17:16:29 +0200

    Commit For Git

---
+++ src/main/java/Core/Locators.java
@@ -0,0 +1,10 @@
+package Core;
+
+import org.openqa.selenium.By;
+
+public class Locators {
+
+    public static By destinationSearchField = By.xpath("//div[@class='c2c15e496d']");
+    public static By setSearchDestination =
+
+}
