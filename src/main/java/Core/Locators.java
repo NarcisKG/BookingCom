56b7fea7d70bbc9d8d8b5f01b3edf353d729a067
NarcisKG <dusan224@gmail.com>
2024-07-08 18:13:58 +0200

    Commit For Git

--- src/main/java/Core/Locators.java
+++ src/main/java/Core/Locators.java
@@ -11,5 +11,9 @@ public class Locators {
     return By.xpath(destinationLocator.replace("*****", destination));}
 
     public static By checkInCheckOutDateField = By.xpath("//div[@class='baca574774']");
-    //public static By
+    public static By checkInCheckOutNextMonth = By.xpath("//button[@class='bf33709ee1 a190bb5f27 dc0e35d124 a746857c37 bb5314095f b81c794d25 f0298d74a8 b4b12cd658 b97282b23a']");
+    public static By checkInCheckOutPreviousMonth = By.xpath("//button[@class='bf33709ee1 a190bb5f27 dc0e35d124 a746857c37 bb5314095f b81c794d25 f0298d74a8 b4b12cd658 b8ee07418a']");
+    public static By setCheckInCheckOutDate (String checkInCheckOutDate){
+        String checkInCheckOutLocator = "(//span[@class='b7df311f35 aa9efce434']//span[text()='30'])[*****]";
+    return  By.xpath(checkInCheckOutLocator.replace("*****", checkInCheckOutDate));}
 }
